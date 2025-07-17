a566038d40fd0bc02b454c191b5cb41d0cb53788
Jeremy Lincoln <bossco28@gmail.com>
2025-07-17 07:20:02 -0400

    Wildcard route and passing data to view

---
+++ resources/views/ninjas/show.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network</title>
+</head>
+<body>
+    <h2>Ninja Network ID: #{{ $id }}</h2>
+</body>
+</html>
