7f0b5752dcb4b8393598c5be00721b57dc6aa947
Jeremy Lincoln <bossco28@gmail.com>
2025-07-18 09:27:08 -0400

    added  pagination

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -10,7 +10,7 @@ class NinjaController extends Controller
     public function index()
     {
         // route --> /ninjas/
-        $ninjas = Ninja::orderBy('created_at', 'desc')->get();
+        $ninjas = Ninja::orderBy('created_at', 'desc')->paginate(10);
 
         // fetch all records from DB and pass to view
         return view('ninjas.index', ["ninjas" => $ninjas]);
